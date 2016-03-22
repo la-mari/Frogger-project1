fa75fe68d69b235714fedddca38e7b7f209e567c
la-mari <karima19@gmail.com>
2016-03-22 15:54:19 -0700

    added 4th lane of cars

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@
 	};
 	
 	//new frog assigned to froggy with ellipse arguments
-	var froggy = new Frog(200, 350, 20, 20);
+	var froggy = new Frog(200, 388, 25, 20);
 
 	// constructor function for cars
 	var Car = function(x, y, width, height, speed){
@@ -49,36 +49,42 @@
 	// };
 
 
-
+	//Collision Detections
 	Car.prototype.collide = function(){
 		// if (this.x === froggy.x){
 		// 	console.log('collision');
 		// 	noLoop();
 		// };
-
 		if((Math.abs((froggy.x + froggy.width/2) - (car1.x + car1.width/2))<(froggy.width/2 + car1.width/2)) && (Math.abs((froggy.y + froggy.height/2) - (car1.y + car1.height/2))<(froggy.height/2 + car1.height/2))){
 			// console.log('collision equation works');
 			noLoop();
 		};
-
 	};
+
+	//Win scenario
+	// if (froggy.x >= )
 
 	
 
 
 	//Lane One
-	var car1 = new Car(0, 250, 50, 35, 1.1);
-	var car2 = new Car (-133,250, 50, 35, 1.1);
-	var car3 = new Car (-267, 250, 50, 35, 1.1);
+	var car1 = new Car(0, 240, 50, 35, 1.1);
+	var car2 = new Car(-133, 240, 50, 35, 1.1);
+	var car3 = new Car(-267, 240, 50, 35, 1.1);
 
 	//Lane Two
-	var car4 = new Car(50, 190, 50, 35, 0.75);
-	var car5 = new Car(-150, 190, 50, 35, 0.75);
+	var car4 = new Car(50, 180, 50, 35, 0.75);
+	var car5 = new Car(-150, 180, 50, 35, 0.75);
 
-	//Land Three
-	var car6 = new Car(0, 130, 50, 35, 1.0);
-	var car7 = new Car(-170, 130, 65, 35, 1.0);
-	var car8 = new Car(-300, 130, 40, 35, 1.0);
+	//Lane Three
+	var car6 = new Car(0, 120, 50, 35, 1.0);
+	var car7 = new Car(-170, 120, 65, 35, 1.0);
+	var car8 = new Car(-300, 120, 40, 35, 1.0);
+
+	//Lane Four
+	var car9 = new Car(0, 60, 60, 35, 1.5);
+	var car10 = new Car(-150, 60, 85, 35, 1.5);
+
 
 
 
@@ -93,16 +99,18 @@
 		// rect(400, 100, 50, 35);
 
 
-		car1.display(); //call display function to show carOne
+		car1.display(); //call display function to show each car
 		car2.display();
 		car3.display();
-		car4.display(); //display carTwo
+		car4.display(); 
 		car5.display();
 		car6.display();
 		car7.display();
 		car8.display();
+		car9.display();
+		car10.display();
 		froggy.display(); //call display to show froggy
-		car1.collide(); 
+		car1.collide(); //call collisiond detection bt froggy and car1
 		
 
 		// if (){ //if carOne collide with frog
