4a3d65dee8333da314187a4e5471a14a53501534
la-mari <karima19@gmail.com>
2016-03-23 11:46:38 -0700

    added working restart button

--- sketch.js
+++ sketch.js
@@ -2,12 +2,13 @@
 	//setup
 	var canvasX = 400;
 	var canvasY = 400;
+	var froggyX = 200;
+	var froggyY = 375;
 
 	function setup() {
 		createCanvas(canvasX, canvasY);
 		rectMode(CENTER);
 	};
-
 
 	//constructor function for frog
 	var Frog = function(x, y, width, height){
@@ -24,7 +25,7 @@
 	};
 	
 	//new frog assigned to froggy with ellipse arguments
-	var froggy = new Frog(200, 360, 25, 20);
+	var froggy = new Frog(froggyX, froggyY, 25, 20);
 
 	// constructor function for cars
 	var Car = function(x, y, width, height, speed){
@@ -38,9 +39,9 @@
 	Car.prototype.display = function(){
 		fill(250, 150, 0) //car's color
 		rect(this.x, this.y, this.width, this.height);
-		if (this.x > canvasX){
+		if (this.x - (this.width/2)> canvasX){
 			// console.log('se paso!');
-			this.x = -this.width;
+			this.x = (this.width/2);
 		} else {
 			this.x += this.speed;
 		};
@@ -72,7 +73,7 @@
 		, new Car(50, 180, 50, 35, 0.75)
 		, new Car(-150, 180, 50, 35, 0.75)
 		, new Car(0, 120, 50, 35, 1.0)
-		, new Car(-170, 120, 65, 35, 1.0)
+		, new Car(-100, 120, 65, 35, 1.0)
 		, new Car(-300, 120, 40, 35, 1.0)
 		, new Car(0, 60, 60, 35, 1.5)
 		, new Car(-150, 60, 85, 35, 1.5))
@@ -112,5 +113,13 @@
 			froggy.x += 20;
 		}
 	};
+
+
+	//restart button
+	$('#restart').on('click', function(){
+	froggy.x = froggyX;
+	froggy.y = froggyY;	
+	loop();
+	});
 
 	
