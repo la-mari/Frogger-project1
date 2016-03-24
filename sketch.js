4c7fc18beac634245fc68f5db3735d93c65cf0e6
la-mari <karima19@gmail.com>
2016-03-23 17:15:58 -0700

    made frog with eyes and cars

--- sketch.js
+++ sketch.js
@@ -5,10 +5,19 @@
 	var froggyX = 200;
 	var froggyY = 375;
 
+
+	//var img;
+	// function preload(){
+	// 	img = loadImage('images/frog1.');
+	// };
+
+
 	function setup() {
-		createCanvas(canvasX, canvasY);
+		var myCanvas = createCanvas(canvasX, canvasY);
 		rectMode(CENTER);
 	};
+
+
 
 	//constructor function for frog
 	var Frog = function(x, y, width, height){
@@ -18,13 +27,22 @@
 		this.height = height;
 	};
 	
-	//this method is used to create frog-ellipse with given parameters
+	//this method to create frog
 	Frog.prototype.display = function() {
 		fill(0, 150, 0);
-		rect(this.x, this.y, this.width, this.height);
+		ellipse(this.x, this.y, this.width, this.height);
+		fill(250, 250, 250);
+		ellipse(this.x + 8, this.y - 10, 10, 7);
+		ellipse(this.x - 8, this.y - 10, 10, 7);
+		fill(0,0,0);
+		ellipse(this.x - 8, this.y - 11, 5, 3.5);
+		ellipse(this.x + 8, this.y -11, 5, 3.5);
 	};
+
+	//comment out fill and rect
+	//replace with image(this.img, this.x, this.y);
 	
-	//new frog assigned to froggy with ellipse arguments
+	//new frog assigned to froggy as a square
 	var froggy = new Frog(froggyX, froggyY, 25, 20);
 
 	// constructor function for cars
@@ -37,8 +55,23 @@
 	};
 
 	Car.prototype.display = function(){
-		fill(250, 150, 0) //car's color
+		//wheels
+		fill(0, 0, 0);
+		rect(this.x + this.width/2 - 8, this.y + this.height/2, 10, 4);
+		rect(this.x - this.width/2 + 8, this.y + this.height/2, 10, 4);
+		rect(this.x + this.width/2 - 8, this.y - this.height/2, 10, 4);
+		rect(this.x - this.width/2 + 8, this.y - this.height/2, 10, 4);
+
+		//body of car
+		fill(250, 150, 0) 
 		rect(this.x, this.y, this.width, this.height);
+
+		//headlights
+		fill(255, 255, 51);
+		ellipse(this.x + this.width/2, this.y-6, 4, 6);
+		ellipse(this.x + this.width/2, this.y+6, 4, 6);
+
+		//collision
 		if (this.x - (this.width/2)> canvasX){
 			// console.log('se paso!');
 			this.x = (this.width/2);
@@ -47,10 +80,10 @@
 		};
 	};
 
-	//Lose Scenario
+	//Lose scenario
 	Car.prototype.collide = function(){
 		if((Math.abs(froggy.x - this.x)<(froggy.width/2 + this.width/2)) && (Math.abs(froggy.y - this.y)<(froggy.height/2 + this.height/2))){
-			console.log('collision equation works');
+			// console.log('collision equation works');
 			alert('You lost!');
 			noLoop();
 		};
@@ -59,9 +92,9 @@
 	//Win scenario
 	var wins = function(){
 		if (froggy.y <= 0){
-		console.log('You Win, Froggy Lives!');
+		// console.log('You Win, Froggy Lives!');
 		noLoop();
-		alert('You Win!');
+		setTimeout(function() { alert("my message"); }, 2000);;
 		};
 	};
 
@@ -83,6 +116,14 @@
 	function draw() {
 		//background here bc draw function continually executes code until stopped. need background redrawn each time car is redrawn
 		background(82, 222, 240);
+
+		//street
+		fill(150,150,150);
+		rect(200, 150, canvasX, 230);
+		//striping
+		fill(255, 255, 51);
+		rect(200, 153, canvasX, 3);
+		rect(200, 147, canvasX, 3);
 	
 		//call display to show froggy
 		froggy.display(); 
@@ -114,6 +155,7 @@
 		}
 	};
 
+ 	$('button').addClass('buttonDesign');
 
 	//restart button
 	$('#restart').on('click', function(){
