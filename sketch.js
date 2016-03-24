2d57303c9928fb2a9b852625c5cbaabb8d994d13
la-mari <karima19@gmail.com>
2016-03-24 09:03:05 -0700

    some styling and win lose on screen

--- sketch.js
+++ sketch.js
@@ -4,20 +4,12 @@
 	var canvasY = 400;
 	var froggyX = 200;
 	var froggyY = 375;
-
-
-	//var img;
-	// function preload(){
-	// 	img = loadImage('images/frog1.');
-	// };
-
+	var lives = 3;
 
 	function setup() {
 		var myCanvas = createCanvas(canvasX, canvasY);
 		rectMode(CENTER);
 	};
-
-
 
 	//constructor function for frog
 	var Frog = function(x, y, width, height){
@@ -38,9 +30,6 @@
 		ellipse(this.x - 8, this.y - 11, 5, 3.5);
 		ellipse(this.x + 8, this.y -11, 5, 3.5);
 	};
-
-	//comment out fill and rect
-	//replace with image(this.img, this.x, this.y);
 	
 	//new frog assigned to froggy as a square
 	var froggy = new Frog(froggyX, froggyY, 25, 20);
@@ -55,17 +44,15 @@
 	};
 
 	Car.prototype.display = function(){
-		//wheels
+		//car wheels
 		fill(0, 0, 0);
 		rect(this.x + this.width/2 - 8, this.y + this.height/2, 10, 4);
 		rect(this.x - this.width/2 + 8, this.y + this.height/2, 10, 4);
 		rect(this.x + this.width/2 - 8, this.y - this.height/2, 10, 4);
 		rect(this.x - this.width/2 + 8, this.y - this.height/2, 10, 4);
-
 		//body of car
 		fill(250, 150, 0) 
 		rect(this.x, this.y, this.width, this.height);
-
 		//headlights
 		fill(255, 255, 51);
 		ellipse(this.x + this.width/2, this.y-6, 4, 6);
@@ -84,33 +71,46 @@
 	Car.prototype.collide = function(){
 		if((Math.abs(froggy.x - this.x)<(froggy.width/2 + this.width/2)) && (Math.abs(froggy.y - this.y)<(froggy.height/2 + this.height/2))){
 			// console.log('collision equation works');
-			alert('You lost!');
 			noLoop();
+			textSize(25);
+			fill(0, 128, 0);
+			text('You Win! Froggy Lives!', 70, 25);
+			lives -= 1;
+		if (lives === 0){
+			textSize(50);
+			fill(255, 26, 26);
+			text('Game Over', 70, 320);
+			lives === 3;
+		}
+
 		};
 	};
+
 
 	//Win scenario
 	var wins = function(){
 		if (froggy.y <= 0){
 		// console.log('You Win, Froggy Lives!');
 		noLoop();
-		setTimeout(function() { alert("my message"); }, 2000);;
+		textSize(25);
+		fill(0, 128, 0);
+		text('You Win! Froggy Lives!', 70, 25);
 		};
 	};
 
 	var cars = [];
 	
-	cars.push(new Car(0, 240, 50, 35, 1.1)
-		, new Car(-133, 240, 50, 35, 1.1)
-		, new Car(-267, 240, 50, 35, 1.1) 
-		, new Car(50, 180, 50, 35, 0.75)
-		, new Car(-150, 180, 50, 35, 0.75)
-		, new Car(0, 120, 50, 35, 1.0)
-		, new Car(-100, 120, 65, 35, 1.0)
-		, new Car(-300, 120, 40, 35, 1.0)
-		, new Car(0, 60, 60, 35, 1.5)
-		, new Car(-150, 60, 85, 35, 1.5))
-	// console.log(cars);
+	cars.push(new Car(0, 240, 50, 35, 1.1),
+		new Car(-133, 240, 50, 35, 1.1),
+		new Car(-267, 240, 50, 35, 1.1),
+		new Car(50, 180, 50, 35, 0.75),
+		new Car(-150, 180, 50, 35, 0.75),
+		new Car(0, 120, 50, 35, 1.0),
+		new Car(-100, 120, 65, 35, 1.0),
+		new Car(-300, 120, 40, 35, 1.0),
+		new Car(0, 60, 60, 35, 1.5),
+		new Car(-150, 60, 85, 35, 1.5))
+		// console.log(cars);
 
 	//draw
 	function draw() {
@@ -131,6 +131,7 @@
 		//call display function to show each car
 		for(var i=0; i<10; i++){
 			cars[i].display();
+			//calling collision function for all cars
 			cars[i].collide();
 		};
 
@@ -143,16 +144,16 @@
 		if (keyCode === UP_ARROW){
 			// console.log("key pressed");
 			froggy.y -= 20;
-		}
+		};
 		if (keyCode === DOWN_ARROW){
 			froggy.y += 20;
-		}
+		};
 		if (keyCode === LEFT_ARROW){
 			froggy.x -= 20;
-		}
+		};
 		if (keyCode === RIGHT_ARROW){
 			froggy.x += 20;
-		}
+		};
 	};
 
  	$('button').addClass('buttonDesign');
